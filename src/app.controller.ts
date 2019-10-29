2a99a23bdaf9e672c69360a6576c252e61b9b913
Felipe-BP <gvv.fel@gmail.com>
2019-10-28 22:30:17 -0300

    feat: initial commit

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, UseInterceptors, UploadedFiles } from '@nestjs/common';
 import { AppService } from './app.service';
+import { FilesInterceptor } from '@nestjs/platform-express';
 
-@Controller()
+@Controller('blockchain')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post('upload')
+  @UseInterceptors(FilesInterceptor('files', 5, { dest: './upload' }))
+  async uploadFiles(@UploadedFiles() files) {
+    const result = await this.appService.hashOfFiles(files);
+    console.log(result);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import * as fs from 'fs';
+import * as crypto from 'crypto';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+
+  constructor() {}
+  
+  hashOfFiles(files) {
+    let hashArrayPromise: Promise<any>[] = [];
+    for (let file of files) {
+      const stream = fs.createReadStream('./' + file.path); // to read file
+      const hash = crypto.createHash('sha256'); // set the algorithm to create hash
+      hash.setEncoding('hex'); // enconding to hexadecimal
+
+      // write the file to the hash object
+      stream.pipe(hash);
+
+      // event dispatched when end read file stream
+      hashArrayPromise.push(
+        new Promise((resolve, reject) => {
+          stream.on('end', () => resolve(hash.read()));
+          stream.on('error', reject);
+        })
+      );
+    }
+    return Promise.all(hashArrayPromise);
   }
+
+  createDestinationTofiles(hashArray: string[]) {
+    console.log(hashArray);
+  }
+
 }
